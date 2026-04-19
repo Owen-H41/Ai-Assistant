ab0fc596d4b413931f08658dba19cb7d09109792
owen.h <owen.hallett@outlook.com>
2026-04-19 12:16:42 +0100

    added token count to output

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    if response.usage_metadata == None:
+        raise RunTimeError("API requst failure likely")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    
     print(response.text)
 
 
