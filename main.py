d5be96812a5d0dd65f955fd697b4788431bb8651
owen.h <owen.hallett@outlook.com>
2026-04-23 16:43:12 +0100

    Added first Function Schema and modified main.py to include prompt and schema list

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from prompts import *
+from call_function import available_functions
 
 
 
@@ -22,15 +23,17 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
     
     if response.usage_metadata == None:
         raise RunTimeError("API requst failure likely")
     if args.verbose == True:
         print(f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-    
-    print(response.text)
-
+    if response.function_calls == None:
+        print(response.text)
+    else:
+        for item in response.function_calls:
+            print(f"Calling function: {item.name}({item.args})")
 
 if __name__ == "__main__":
     main()
