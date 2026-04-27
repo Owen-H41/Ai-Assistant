451ed78277b3d7297d943d38694f23129d19a1d7
owen.h <owen.hallett@outlook.com>
2026-04-27 13:38:38 +0100

    Modified prompt to be more precise

--- prompts.py
+++ prompts.py
@@ -1,6 +1,8 @@
 system_prompt = """
 You are a helpful AI coding agent.
 
+you need to find and identify why some code isn't working as intended and attempt to fix the code to work as the user requests using the provided functions
+
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
